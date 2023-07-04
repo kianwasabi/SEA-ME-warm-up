836b15d112d3c1bc88f4dcb37ab8a1160d856ece
kianwasabi <55065075+kianwasabi@users.noreply.github.com>
2023-07-04 19:30:00 +0200

    20230704
    Day1 - Module00/Exercise02

---
+++ Modules/Module00/Exercise02/include/convert.h
@@ -0,0 +1,20 @@
+#ifndef CONVERT
+#define CONVERT
+
+#include <iostream>
+#include <iostream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+class Convert{
+public:
+  Convert(){}
+  ~Convert(){}
+};
+
+string convertUppercase(const string& str);
+string convertLowercase(const string& str);
+
+#endif //CONVERT
+
---
+++ Modules/Module00/Exercise02/main.cpp
@@ -0,0 +1,27 @@
+#include "include/convert.h"
+
+int main(int argc, char* argv[]) {
+    //Print to terminal that argv does not include all needed information (/path/command/string)
+    if (argc < 3) {
+        cout << "Usage: ./convert {command} {string}" << endl;
+    }
+    //Extract cmd input (/path/command/string)
+    string file_path = argv[0];
+    string command = argv[1]; 
+    string str = argv[2];
+    //Don't forget that a string input can be separated by spaces/tablogs.
+    for(int i = 3; i<argc; i++){
+        //str.append(" ");
+        str.append(argv[i]);
+    }
+    //Call functions for command "up" oder "down" and print to terminal
+    string result;
+    if (command == "up") {
+        result = convertUppercase(str);
+    } else if (command == "down") {
+        result = convertLowercase(str);
+    } else {
+        cout << "Invalid command" << endl;
+    }
+    cout << result << endl;
+}
